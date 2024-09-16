d568ef90b246e56f273d41cb0884738a71642de9
Ruan Gomes Rodrigues <ruangomesnk2617@gmail.com>
2024-09-16 09:03:19 -0300

    implementando a função buscarporTitulo

--- api.js
+++ api.js
@@ -1,19 +1,11 @@
-async function fetchSeriesByTitle(titulo) {
-    const response = await fetch(`http://localhost:3000/series?titulo=${encodeURIComponent(titulo)}`);
-    if (!response.ok) {
-        throw new Error('Network response was not ok')
+async function buscarporTitulo(titulo) {
+    const resposta = await fetch(`http://localhost:3000/series?titulo=${encodeURIComponent(titulo)}`)
+    if(!resposta.ok){
+        console.log("Mídia não encontrada")
+        return
     }
-    const data = await response.json()
+    const data = await resposta.json()
     return data
 }
 
-async function fetchSeriesByPage(page, limit) {
-    const response = await fetch(`http://localhost:3000/series?pagina=${page}&limite=${limit}`);
-    if (!response.ok) {
-        throw new Error('Network response was not ok')
-    }
-    const data = await response.json()
-    return data
-}
 
-export { fetchSeriesByTitle, fetchSeriesByPage }
