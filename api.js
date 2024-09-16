9ab4bd4c6cd125196fc5d3e0637f3c17c47212ed
Ruan Gomes Rodrigues <ruangomesnk2617@gmail.com>
2024-09-16 09:24:24 -0300

    adicionando a função buscarConteudo

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-async function buscarporTitulo(titulo) {
+async function buscarPorTitulo(titulo) {
     const resposta = await fetch(`http://localhost:3000/series?titulo=${encodeURIComponent(titulo)}`)
     if(!resposta.ok){
         console.log("Mídia não encontrada")
@@ -8,4 +8,18 @@ async function buscarporTitulo(titulo) {
     return data
 }
 
+async function buscarConteudo(pagina, limite) {
+    const resposta = await fetch(`http://localhost:3000/series?pagina=${pagina}&limite=${limite}`)
+    if(!resposta.ok){
+        console.log("Mídia não encontrada")
+        return
+    }
+    const data = await resposta.json()
+    return data
+}
 
+console.log(await buscarConteudo(2, 5))
+
+
+
+/*export{buscarConteudo, buscarPorTitulo}*/
